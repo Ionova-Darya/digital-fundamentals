6fa4fe6de8722ad0da23744c4e28f82d5734952a
Ионова Дарья <ionova.dd@edu.spbstu.ru>
2025-11-07 14:06:28 +0300

    Update analyser1.sh
    добавлен текст скрипта

--- scripts/analyser1.sh
+++ scripts/analyser1.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+for i in $(ls $1)
+do
+  for j in $(cat41$i)
+  do
+   name=$(echo $j | cut -d "." -f1)
+   age=$(echo $j | cut -d "." -f2)
+   email=$(echo $j | cut -d "." -f3)
+   echo "Name: $name | Age: $age | Email: $email"
+  done
+done
